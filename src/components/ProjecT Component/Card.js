0e0140e5e686bd4c1430840b76e858f0aa9f40cd
Subhajit Ghosh <subhajitstd07@gmail.com>
2022-10-23 01:45:39 +0530

    Made projects section and added some of my created projects

--- src/components/ProjecT Component/Card.js
+++ src/components/ProjecT Component/Card.js
@@ -1,48 +1,54 @@
 import React from 'react'
 
-function Card({project_name,creation_date,}) {
+function Card({projectid,project_name,project_images,project_heading,project_description,project_github_link,project_deployed_link}) {
   return (
+    <>
+    
     <div className="card card-side  shadow-xl mb-5 mt-5 w-full bg-[#102a50]">
         <div className="h-96 carousel carousel-vertical rounded-box">
-            <div className="carousel-item h-full">
-                <img src="./projectimages/img1.jpeg" alt="project_image"  className='object-cover min-w-3xl sm:w-full'/>
-            </div> 
-            <div className="carousel-item h-full">
-                <img src="./projectimages/img3.jpeg" alt="project_image"  className='object-cover min-w-3xl sm:w-full'/>
-            </div> 
-            <div className="carousel-item h-full">
-                <img src="./projectimages/img4.jpeg" alt="project_image"  className='object-cover min-w-3xl sm:w-full'/>
-            </div> 
-            <div className="carousel-item h-full">
-                <img src="./projectimages/img5.jpeg" alt="project_image"  className='object-cover min-w-3xl sm:w-full'/>
-            </div> 
-            <div className="carousel-item h-full">
-                <img src="./projectimages/img6.jpeg" alt="project_image"  className='object-cover min-w-3xl sm:w-full'/>
-            </div> 
-            <div className="carousel-item h-full">
-                <img src="./projectimages/img7.jpeg" alt="project_image"  className='object-cover min-w-3xl sm:w-full'/>
-            </div> 
+            {/* Rendering all of my Project imgages one by one */}
+            {
+                project_images.length > 0 ? project_images.map((images,index)=>{
+                    return <div className="carousel-item h-full" key={index}>
+                    <img src={images.toString()} alt="project_image"  className='object-cover min-w-3xl sm:w-full'/>
+                </div> 
+                }) : "No Images"
+            }
         </div>
         <div className="card-body">
-        <h2 className="card-title text-cyan-300">e-marcket</h2>
-        <div className='text-gray-400 text-justify max-w-xl h-36 sm:h-44 overflow-y-scroll ' >
-            It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+        <h2 className="card-title text-cyan-300">{project_name}</h2>
+        <div className="card-actions justify-start">
+            <a href={`${project_github_link}`} target="_blank" className="link link-secondary"><i className="fa-brands fa-github"></i></a>
+            <a href={`${project_deployed_link}`} target="_blank" className="link link-secondary"><i className="fa-solid fa-link"></i></a>
         </div>
-        <div className="card-actions justify-end">
-            <a href='https://github.com/subhajit20/e-marcket' target="_blank" className="link link-secondary"><i class="fa-brands fa-github"></i></a>
-            <a href='https://e-marcket.vercel.app/' target="_blank" className="link link-secondary"><i class="fa-solid fa-link"></i></a>
-        </div>
-        
+        <div className='py-10'></div>
         <div className="card-actions justify-end text-gray-200">
-            <div className="badge bg-transparent outline-none"><i class="fa-brands fa-github"></i></div> 
-            <div className="badge bg-transparent outline-none"><i class="fa-brands fa-react"></i></div>
-            <div className="badge bg-transparent outline-none"><i class="fa-brands fa-node-js"></i></div>
-            <div className="badge bg-transparent outline-none"><i class="fa-solid fa-code"></i></div>
-            <div className="badge bg-transparent outline-none"><i class="fa-brands fa-css3"></i></div>
-            <div className="badge bg-transparent outline-none"><i class="fa-brands fa-paypal"></i></div>
+            <div className="badge bg-transparent outline-none"><i className="fa-brands fa-github"></i></div> 
+            <div className="badge bg-transparent outline-none"><i className="fa-brands fa-react"></i></div>
+            <div className="badge bg-transparent outline-none"><i className="fa-brands fa-node-js"></i></div>
+            <div className="badge bg-transparent outline-none"><i className="fa-solid fa-code"></i></div>
+            <div className="badge bg-transparent outline-none"><i className="fa-brands fa-css3"></i></div>
+            <div className="badge bg-transparent outline-none"><i className="fa-brands fa-paypal"></i></div>
         </div>
         </div>
   </div>
+  
+    <div className="bg-lightblue px-4">
+        <div className="mx-auto max-w-6xl flex flex-col md:flex-row">
+            <h2 className="mr-8 w-full md:w-1/3 text-3xl font-extrabold leading-9 text-cyan-500">
+                {projectid} : {project_heading}
+            </h2>
+            <dl className="w-full md:w-2/3">
+                <dd className="mb-16">
+                    <p className='text-gray-300'>
+                        {project_description}
+                    </p>
+                </dd>
+            </dl>
+        </div>
+    </div>
+
+  </>
   )
 }
 
--- src/components/ProjecT Component/ProjectTemplate.js
+++ src/components/ProjecT Component/ProjectTemplate.js
@@ -1,12 +1,21 @@
 import React from 'react';
-
 import Card from './Card';
+import ProcjetDetails from '../ProcjetDetails.json'
+
 function ProjectTemplate() {
+  const myprojects = ProcjetDetails.AllProjects;
   return (
     <div id="work" className='flex flex-wrap justify-center p-5 bg-[#0a192f] h-auto relative'>
-        
+        <h6 className="font-extrabold leading-9 text-cyan-500">
+                <span className='text-cyan-500'>02</span>  <span className='text-gray-500'>Some Things I’ve Built -------</span>
+          </h6>
         <div className='flex-col'>
-            <Card/>
+          {/* Rendering all of my Projects */}
+            {
+              myprojects.map((projects,index)=>{
+                return <Card key={index} projectid={projects.id} project_name={projects.project_name} project_images={projects.project_images} project_heading={projects.project_heading} project_description={projects.project_description} project_github_link={projects.project_github_link} project_deployed_link={projects.project_deployed_link}/>
+              })
+            }
         </div>
     </div>  
   )
