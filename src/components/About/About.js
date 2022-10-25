9fce7b31dfdfd7c7c2687df546c314dc96464d9e
Subhajit Ghosh <99127578+subhajit20@users.noreply.github.com>
2022-10-25 09:59:13 +0530

    Update About.js

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -15,10 +15,10 @@ function About() {
               </p>
             </div>
             <p className="text-base text-gray-300 md:text-lg text-justify">
-            Hello! My name is Subhajit Ghosh and I enjoy creating things that live on the internet. My interest in web development started back in 2012 when I decided to try editing custom Tumblr themes — turns out hacking together a custom reblog button taught me a lot about HTML & CSS!
+            Hello! My name is Subhajit Ghosh and I enjoy creating things that live on the internet. My interest in web development started back in 2021 when I decided to try editing custom Tumblr themes — turns out hacking together a custom reblog button taught me a lot about HTML & CSS!
             </p>
             <p className="text-base text-gray-300 md:text-lg text-justify">
-            Fast-forward to today, and I’ve had the privilege of working at an advertising agency, a start-up, a huge corporation, and a student-led design studio. My main focus these days is building accessible, inclusive products and digital experiences at Upstatement for a variety of clients.
+            Fast-forward to today, and I’ve had the privilege of working at an advertising agency . My main focus these days is building accessible, inclusive products and digital experiences at Upstatement for a variety of clients.
             </p>
             <div className='relative top-10'>
               <p className="text-base text-cyan-500 md:text-lg text-justify">
